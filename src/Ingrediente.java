4e24fea25ec9b6491e1288f33a3cd66161364df2
0IIIIM0 <ignac23med@gmail.com>
2022-03-10 11:32:37 -0600

    Initial commit

---
+++ src/Ingrediente.java
@@ -0,0 +1,6 @@
+public class Ingrediente {
+    private String nombre;
+    private boolean salado;
+    private double peso; // kg,l
+
+}
---
+++ src/Main_Recetario.java
@@ -0,0 +1,5 @@
+public class Main_Recetario {
+    public static void main(String[] args) {
+
+    }
+}
