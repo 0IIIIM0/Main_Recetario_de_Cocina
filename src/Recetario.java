aa671112d817a87376ebc7092a1aa9c84ceb4ee7
0IIIIM0 <ignac23med@gmail.com>
2022-03-14 11:42:19 -0600

    create geter and setters in Recetario

--- src/Recetario.java
+++ src/Recetario.java
@@ -1,6 +1,37 @@
 import java.util.ArrayList;
 
 public class Recetario {
-    ArrayList<Recetario>recetarios;
+    private ArrayList<Recetario>recetarios;
+    private String titulo; //nacho recetas
+    private String autor; //nacho
 
+    public ArrayList<Recetario> getRecetarios() {
+        return recetarios;
+    }
+
+    public void setRecetarios(ArrayList<Recetario> recetarios) {
+        this.recetarios = recetarios;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Recetario(ArrayList<Recetario> recetarios, String titulo, String autor) {
+        this.recetarios = recetarios;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
 }
